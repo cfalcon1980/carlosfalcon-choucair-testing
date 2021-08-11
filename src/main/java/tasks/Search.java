a266b1333bbadc86ddc9fdcba3332efca5efb364
Carlos <falconcar1980@gmail.com>
2021-08-10 21:25:35 -0500

    Corrigiendo valor quemado

--- src/main/java/tasks/Search.java
+++ src/main/java/tasks/Search.java
@@ -22,7 +22,7 @@ public class Search implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(Click.on(SearchCoursePage.BUTTON_UC),
-                Enter.theValue("course").into(SearchCoursePage.INPUT_COURSE),
+                Enter.theValue(course).into(SearchCoursePage.INPUT_COURSE),
                 Click.on(SearchCoursePage.BUTTON_GO),
                 Click.on(SearchCoursePage.SELECT_COURSE),
                 Click.on(SearchCoursePage.NAME_COURSE)
